42ec16067131769a46aee153d477b734c03a22f6
Nobime <romeo.nobime@mailo.com>
2022-08-16 09:27:46 +0200

    button.js get classname property

--- src/scripts/button.js
+++ src/scripts/button.js
@@ -1,3 +1,3 @@
-export function button(id, text) {
-  return /*html*/ `<button class="movie__button movie__button--black" id=${id} type="button">${text}</button>`;
+export function button(id, className) {
+  return /*html*/ `<button class=${className} id=${id} type="button"></button>`;
 }
